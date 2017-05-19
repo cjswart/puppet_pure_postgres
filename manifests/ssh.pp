ed793aa2e601ea765bb23b83538ab1e521d07df1
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-05-19 17:16:45 +0200

    I have moved some ssh stuff from pure_repmgr to pure_postgres module, so that both pure_repmgr and pure_barman can use it.

---
+++ manifests/ssh.pp
@@ -0,0 +1,36 @@
+# == Class: pure_postgres::ssh
+#
+# Configure a replicated cluster with repmgr from pure repo 
+class pure_postgres::ssh
+(
+  $tags = undefined,
+)
+{
+
+  @@sshkey { $facts['fqdn']:
+    type => ecdsa-sha2-nistp256,
+    key  => $::sshecdsakey,
+    tag  => $tags,
+  }
+
+  @@sshkey { "${facts['fqdn']}_${facts['networking']['ip']}":
+    name => $facts['networking']['ip'],
+    type => ecdsa-sha2-nistp256,
+    key  => $::sshecdsakey,
+    tag  => $tags,
+  }
+
+  if $facts['fqdn'] != $facts['hostname'] {
+    @@sshkey { "${facts['fqdn']}_${facts['hostname']}":
+      name => $facts['hostname'],
+      type => ecdsa-sha2-nistp256,
+      key  => $::sshecdsakey,
+      tag  => $tags,
+    }
+  }
+
+  $tags.each | String $tag | {  
+    Sshkey <<| tag == $tag |>>
+  }
+}
+
