d37ac7b0d82964daa8024e4c8cd30c72b0ab1028
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-05-20 02:27:46 +0200

    Added feature for also installing support package to repo class

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,10 +3,12 @@
 # Installs pure repo
 class pure_postgres::repo
 (
-  $repo                 = $pure_postgres::params::repo,
-  $version              = $pure_postgres::params::version,
-  $repo_package         = $pure_postgres::params::repo_package,
+  $repo            = $pure_postgres::params::repo,
+  $version         = $pure_postgres::params::version,
+  $repo_package    = $pure_postgres::params::repo_package,
+  $support_package = false,
 ) inherits pure_postgres::params
+# inherits is added to force defining pure_postgres::params. WIthout it, $version might be undef and not $pure_postgres::params::version
 {
   $dist = $::operatingsystem ?
   {
@@ -21,6 +23,12 @@ class pure_postgres::repo
     descr    => 'Postgres PURE',
     enabled  => 1,
     gpgcheck => 0
+  }
+
+  if $support_package {
+    Package { $support_package:
+      require => Yumrepo['PostgresPURE'],
+    }
   }
 
 }
